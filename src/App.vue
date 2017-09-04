494c563fe000d038d0a9396a6d3f9920510427c7
xiangjun9988 <xiangjun9988@163.com>
2017-09-04 18:52:19 +0800

    首页布局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,53 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- 头部导航 -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <a class="navbar-brand" href="#">抽奖系统</a>
+        <ul class="nav navbar-nav">
+            <li class="active">
+                <a href="#">首页</a>
+            </li>
+            <li>
+                <a href="#">中奖名单</a>
+            </li>
+             <li>
+                <a href="#">参与抽奖者</a>
+            </li>
+            <li>
+                <a href="#">学院喜报</a>
+            </li>
+        </ul>
+        <div class="btn-group pull-right bw-r">
+          <button type="button" class="btn btn-default">抽奖报名</button>
+          <button type="button" class="btn btn-default">参与名单</button>
+          <button type="button" class="btn btn-default">设置</button>
+      </div>
+    </nav>
+    <!-- 彩蛋效果 -->
+    <canvas-box></canvas-box>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import CanvasBox from '@/components/CanvasBox'
+import Home from '@/components/Home'
 export default {
-  name: 'app'
+  name: 'app',
+  components:{
+    CanvasBox,
+    Home
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0px;
+  padding: 0px;
+}
+.navbar .bw-r{
+  margin-top:10px;
+  margin-right:20px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,84 @@
+<template>
+  <section class="home">
+  <div class="row">
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      <div class="panel panel-primary">
+        <div class="page-header">
+          <h1>参与名单<small>100人</small></h1>
+        </div>
+
+        <div class="panel-body" style="height:400px;overflow:auto;">
+          <ul class="list-group">
+            <li class="list-group-item" v-for="item in 100">张三{{item}} 182****0698</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      <div class="panel panel-primary">
+        <div class="page-header">
+          <h1>学院利好</h1>
+        </div>
+
+        <div class="panel-body">
+          <ul class="list-group">
+            <li class="list-group-item">Item 1</li>
+            <li class="list-group-item">Item 2</li>
+            <li class="list-group-item">Item 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      <div class="panel panel-primary">
+        <div class="page-header">
+          <h1>奖品信息</h1>
+        </div>
+
+        <div class="panel-body">
+          <ul class="list-group">
+            <li class="list-group-item" v-for="info in jpInfo">{{info}}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      <div class="panel panel-primary">
+        <div class="page-header">
+          <h1>参与名单<small>100人</small></h1>
+        </div>
+
+        <div class="panel-body">
+          <ul class="list-group">
+            <li class="list-group-item">Item 1</li>
+            <li class="list-group-item">Item 2</li>
+            <li class="list-group-item">Item 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            userList:['张三'],
+            jpInfo:["100元3名","50元5名","20元50名","10元100名","5元292名"]
+        }
+    }
+}
+</script>
+
+<style>
+.home{
+    padding:0 20px;
+    margin-top:45px;
+}
+.page-header{
+    margin: 10px 0 10px;
+}
+</style>
